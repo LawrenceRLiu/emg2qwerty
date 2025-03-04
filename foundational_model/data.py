255e7b767bc79df4accfe553d1a3bf07d0d4f53c
HighSpeeds <lawrencerliu@ucla.edu>
2025-03-03 20:27:23 -0800

    finished readme, adding loss

--- foundational_model/data.py
+++ foundational_model/data.py
@@ -1,25 +1,69 @@
 import emg2qwerty.data
 from dataclasses import dataclass
+from typing import List, Tuple, Dict
+import torch
+
+
+# Some thing I noticed was that for the first index, the output shape will be different because of padding for the windowed dataset, so padding should be set to zero unless there is a bug
 
 @dataclass
-class PretrainSingleChannel_emg2qwerty(emg2qwerty.data.WindowedEMGDataset):
+class PretrainChannelWise_emg2qwerty(emg2qwerty.data.WindowedEMGDataset):
 
     n_channels = 32 #hardcoded because I didn't care to get this from the config
     def __len__(self):
-        if hasattr(self, "n_channels"):
+        return super().__len__()*self.n_channels
+    
+    def __getitem__(self, idx) -> Dict[str:torch.Tensor]:
+        """get item from the dataset
 
-            return super().__len__()*self.n_channels
-        else:
-            return super().__len__()
-    def __getitem__(self, idx):
+        Returns:
+            output: Dict[str:torch.Tensor]: a dictionary containing the emg data, of shape (1, window_size)
+            There is a one so that we unify the shape of the output between different methods of getting the data
+        """
 
-        emg,*_ = super().__getitem__(idx//self.n_channels)
-        print(emg.shape)
+        emg,*_ = super().__getitem__(idx//self.n_channels) #shape of (window_size, 2, 16)
         i = idx%self.n_channels
         hand = i % 2
         channel = i // 2
-        data = emg[:, hand, channel
-                   ]
-        return {"emg":data}
+        data = emg[:, hand, channel]
+        return {"emg":data.unsqueeze(0)}
     
 
+
+@dataclass
+class PretrainHandWise_emg2qwerty(emg2qwerty.data.WindowedEMGDataset):
+
+    n_hands = 2 #2 hands, I hope!
+    def __len__(self):
+        return super().__len__()*self.n_hands
+    def __getitem__(self, idx)-> Dict[str:torch.Tensor]:
+        """get item from the dataset
+
+        Returns:
+            output: Dict[str:torch.Tensor]: a dictionary containing the emg data, of shape (16, window_size)
+        """
+
+        emg,*_ = super().__getitem__(idx//self.n_hands) #shape of (window_size, 2, 16)
+        hand = idx % self.n_hands
+        data = emg[:, hand, :].T
+        return {"emg":data}
+
+
+@dataclass
+class Pretrain_emg2qwerty(emg2qwerty.data.WindowedEMGDataset):
+    flatten: bool = True #whether to flatten the data
+
+    def __getitem__(self, idx)-> Dict[str:torch.Tensor]:
+        """get item from the dataset
+
+        Returns:
+            output: Dict[str:torch.Tensor]: a dictionary containing the emg data, of shape (16, 2, window_size) if flatten is False, else (16*2, window_size)
+        """
+
+        emg,*_ = super().__getitem__(idx)
+        if self.flatten:
+            data = emg.reshape(-1, emg.shape[-1])
+        else:
+            data = emg
+        return {"emg":data}
+    
--- foundational_model/readme.md
+++ foundational_model/readme.md
@@ -1,1 +1,25 @@
-General Idea, pretrain a model on the spectogram of each individual channel using a ViTMAE format.
+General Idea. 
+
+Use a model trained on either the spectogram of an EMG in a self supervised manner as a foundational model.
+This will embed the spectogram into a lower dimensional space. 
+
+Then for the downstream task of keystroke prediction, we can freeze the 
+weights of the foundational model and train smaller MLP or some form of classifier on top of it. Hopefully
+this will allow us to train the downstream task with less data. Furthermore for the paper, we can sell this as a 
+foundational model that can be used for other tasks as well.
+
+TODOs and potential research directions:
+- [ ] Move the loss fns and spectogram transforms to seperate classes 
+- [ ] Finish two trainers, one for a foundational model and one for a downstream task, these will be based on the huggingface trainers
+- [ ] Use Hydra for config management (potentially)
+- [ ] Experiment with different foundational architectures models, for example:
+    - [ ] Autencoder (CNN)
+    - [ ] VAE (CNN) (Daniel)
+    - [ ] ViTMAE (Lawrence)
+    - [ ] VLLM (Manik)
+- [ ] Experiment with different loss functions and training stuff, for example:
+    - [ ] Channel wise foundational model (compress each channel separately)
+    - [ ] Handwise foundational model (compress each hand separately)
+    - [ ] Moving beyond MSE for Spectogram reconstruction loss, for example VGG loss
+- [ ] Agree on a format between the foundational model and the downstream task
+- [ ] Incoporate more data, for example emg2pose dataset and more downstream tasks (This can be saved for post paper)
